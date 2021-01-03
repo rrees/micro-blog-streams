32bd5f2a9d694974fa483568108f8a4343792bc6
Robert Rees <shudderfix@gmail.com>
2021-01-03 17:24:53 +0000

    Creates a Favicon and formats the dates in a filter

--- app/filters.py
+++ app/filters.py
@@ -1,3 +1,5 @@
+import datetime
+
 import mistune
 
 markdown_renderer = mistune.Markdown()
@@ -5,6 +7,10 @@ markdown_renderer = mistune.Markdown()
 def markdown(markdown):
 	return markdown_renderer(markdown)
 
+def iso_date(a_date):
+	return a_date.isoformat(timespec='seconds')
+
 custom_filters = (
 	('markdown', markdown),
+	('iso_date', iso_date),
 )
