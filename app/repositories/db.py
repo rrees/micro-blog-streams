4680eac64c57f692b1815bf36dc6fb255a564e66
Robert Rees <shudderfix@gmail.com>
2021-03-28 23:57:10 +0100

    Uses suggested URL rewriting for SQL Alchemy compatibility

--- app/repositories/db.py
+++ app/repositories/db.py
@@ -2,7 +2,11 @@ import os
 
 import dataset
 
-connection = dataset.connect(os.environ["DATABASE_URL"])
+_db_url = os.environ["DATABASE_URL"]
+
+_connection_url = _db_url.replace("postgres://", "postgresql://", 1) if _db_url.startswith("postgres:") else _db_url
+
+connection = dataset.connect(_connection_url)
 
 def connect():
-	return dataset.connect(os.environ["DATABASE_URL"])
+	return dataset.connect(_connection_url)
