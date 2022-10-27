ee50a8f6019908a60509f5d888bc106b485b3556
Robert Rees <shudderfix@gmail.com>
2022-10-27 20:47:20 +0100

    Updates to the context block method more universally

--- app/repositories/db.py
+++ app/repositories/db.py
@@ -4,9 +4,14 @@ import dataset
 
 _db_url = os.environ["DATABASE_URL"]
 
-_connection_url = _db_url.replace("postgres://", "postgresql://", 1) if _db_url.startswith("postgres:") else _db_url
+_connection_url = (
+    _db_url.replace("postgres://", "postgresql://", 1)
+    if _db_url.startswith("postgres:")
+    else _db_url
+)
 
 connection = dataset.connect(_connection_url)
 
+
 def connect():
-	return dataset.connect(_connection_url)
+    return dataset.connect(_connection_url)
--- app/repositories/posts.py
+++ app/repositories/posts.py
@@ -16,20 +16,25 @@ def post_mapper(post):
     if not post:
         return None
     return models.BlogPost(
-        id=post['id'],
-        title=post['title'],
-        content=post['content'],
-        tags=post['tags'],
-        updated=post['updated'],
+        id=post["id"],
+        title=post["title"],
+        content=post["content"],
+        tags=post["tags"],
+        updated=post["updated"],
     )
 
 
 def all():
-    return [post_mapper(r) for r in table]
+    with connect() as tx:
+        return [post_mapper(r) for r in tx[TABLENAME]]
 
 
 def latest(limit=20):
-    return [post_mapper(r) for r in table.find(_limit=limit, order_by='-updated')]
+    with connect() as tx:
+        return [
+            post_mapper(r)
+            for r in tx[TABLENAME].find(_limit=limit, order_by="-updated")
+        ]
 
 
 def create(title, content, tags=None, topic_id=None, url=None):
@@ -37,27 +42,28 @@ def create(title, content, tags=None, topic_id=None, url=None):
         post_data = {"title": title, "content": content}
 
         if tags:
-            post_data['tags'] = tags
+            post_data["tags"] = tags
 
         if url:
-            post_data['url'] = url
+            post_data["url"] = url
 
         post_id = tx[TABLENAME].insert(post_data)
 
         if topic_id:
-            topic_posts = tx['topic_posts']
-            topic_posts.insert({'blog_post_id': post_id, 'topic_id': topic_id})
+            topic_posts = tx["topic_posts"]
+            topic_posts.insert({"blog_post_id": post_id, "topic_id": topic_id})
 
         return post_id
 
 
 def post(post_id):
-    db = connect()
-    return db[TABLENAME].find_one(id=post_id)
+    with connect() as tx:
+        return tx[TABLENAME].find_one(id=post_id)
 
 
 def recent():
-    return queries.read(table.find(order_by=['-updated']), post_mapper)
+    with connect() as tx:
+        return queries.read(tx[TABLENAME].find(order_by=["-updated"]), post_mapper)
 
 
 def by_topic(topic_id, recent=True):
@@ -74,12 +80,13 @@ def by_topic(topic_id, recent=True):
 def update_post(new_post_data):
 
     with connect() as tx:
-        table.update(new_post_data, ['id'])
+        table.update(new_post_data, ["id"])
 
 
 def with_title_matching(search_text):
     with connect() as db:
-        return map(post_mapper, table.find(title={'ilike': f"%{search_text}%"}))
+        return map(post_mapper, table.find(title={"ilike": f"%{search_text}%"}))
+
 
 def with_tag(tag):
     query = "SELECT * FROM blogpost WHERE :tag = ANY(tags)"
