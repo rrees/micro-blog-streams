3bd97de9b2a75e71d268c73821896f8f9bbceac3
Robert Rees <shudderfix@gmail.com>
2023-02-25 13:10:55 +0000

    Switches the posts over to a navigation page as well

--- app/handlers/navigation.py
+++ app/handlers/navigation.py
@@ -6,7 +6,13 @@ from app.auth_password.decorators import login_required
 def navigation_topics():
     return flask.render_template("navigation/topics.html")
 
+@login_required
+def navigation_posts():
+    return flask.render_template("navigation/posts.html")
+
+
 
 navigation_routes = [
     ('/navigation/topics', 'navigation_topics', navigation_topics, ['GET']),
+    ('/navigation/posts', 'navigation_posts', navigation_posts, ['GET']),
 ]
