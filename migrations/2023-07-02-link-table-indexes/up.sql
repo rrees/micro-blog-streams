e69d82394694932fc7ec79f385c6e529e542fa10
Robert Rees <shudderfix@gmail.com>
2023-07-02 10:46:33 +0100

    Indexes columns that are used in join queries

---
+++ migrations/2023-07-02-link-table-indexes/up.sql
@@ -0,0 +1,3 @@
+
+CREATE INDEX topic_posts_posts ON topic_posts(blog_post_id);
+CREATE INDEX topic_posts_topics ON topic_posts(topic_id);
---
+++ migrations/2023-07-02-post-timestamp-indexes/up.sql
@@ -0,0 +1,3 @@
+
+CREATE INDEX posts_created_idx ON blogpost(created);
+CREATE INDEX posts_updates_idx ON blogpost(updated);
