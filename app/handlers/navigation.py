73dfefeb0e3cbfe546dcb593253f82a392b0a4e1
Robert Rees <shudderfix@gmail.com>
2023-12-05 20:40:09 +0000

    Updates the navigation and tries out a combined menu page

--- app/handlers/navigation.py
+++ app/handlers/navigation.py
@@ -2,17 +2,24 @@ import flask
 
 from app.auth_password.decorators import login_required
 
+
 @login_required
 def navigation_topics():
     return flask.render_template("navigation/topics.html")
+
 
 @login_required
 def navigation_posts():
     return flask.render_template("navigation/posts.html")
 
 
+@login_required
+def navigation_menu():
+    return flask.render_template("navigation/menu.html")
+
 
 navigation_routes = [
-    ('/navigation/topics', 'navigation_topics', navigation_topics, ['GET']),
-    ('/navigation/posts', 'navigation_posts', navigation_posts, ['GET']),
+    ("/navigation/topics", "navigation_topics", navigation_topics, ["GET"]),
+    ("/navigation/posts", "navigation_posts", navigation_posts, ["GET"]),
+    ("/navigation/menu", "navigation_menu", navigation_menu, ["GET"]),
 ]
