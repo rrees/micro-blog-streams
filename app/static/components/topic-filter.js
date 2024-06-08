8c709888cdc5e11aca254394276db4386ee241e2
Robert Rees <shudderfix@gmail.com>
2024-06-08 08:07:27 +0100

    Creates a working web component that filters the list

--- app/static/components/topic-filter.js
+++ app/static/components/topic-filter.js
@@ -13,6 +13,24 @@ class TopicFilter extends HTMLElement {
 		const filter = document.querySelector('#topic-filter');
 
 		wrapper.removeAttribute('hidden');
+
+		filter.addEventListener('input', (event) => {
+			console.log('Filter change', event.target.value);
+			const filterRegularExpression = new RegExp(event.target.value, "i");
+
+			const topics = document.querySelectorAll('.topic');
+			const topicLinks = document.querySelectorAll('.topic-link');
+
+			for(const topic of topics) {
+				topic.removeAttribute('hidden');
+			}
+
+			for(const link of topicLinks) {
+				if(!filterRegularExpression.test(link.text)) {
+					link.parentElement.setAttribute('hidden', 'hidden');
+				}
+			}
+		})
 	}
 }
 
