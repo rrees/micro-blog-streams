b3fbebd74e502e0d67aecb271de001ee3a2860d3
Robert Rees <shudderfix+git@gmail.com>
2026-09-06 08:48:37 +0100

    Seems to have a working form wrapper

--- app/static/components/FormWrapper.js
+++ app/static/components/FormWrapper.js
@@ -1,4 +1,65 @@
 class FormWrapper extends HTMLElement {
+	constructor() {
+		super();
+
+		this.form = this.querySelector('form');
+
+		this.form.addEventListener('submit', this);
+
+		this.statusAttributeName = 'form-submitting';
+	}
+
+	disable() {
+		this.setAttribute(this.statusAttributeName, '');
+	}
+
+	enable() {
+		this.removeAttribute(this.statusAttributeName);
+	}
+
+	isDisabled() {
+		return this.hasAttribute(this.statusAttributeName);
+	}
+
+
+	handleEvent(event) {
+		console.log(event);
+		this[`on${event.type}`](event);
+	}
+
+	async onsubmit(event) {
+		event.preventDefault();
+
+		console.log('Form submitting');
+
+		if(this.isDisabled()) {
+			console.log('Form disabled')
+			return;
+		}
+
+		this.disable();
+
+		try {
+			const {action, method} = this.form;
+
+			console.log(action, method)
+
+			const response = await fetch(action, {
+				method: method,
+				body: new FormData(this.form)
+			});
+
+			const data = response;
+
+			console.log(data);
+
+			window.location.href = data.url;
+
+		} finally {
+			this.enable();
+		}
+	}
+
 	connectedCallback() {
 		console.log('FormWrapper connected');
 	}
